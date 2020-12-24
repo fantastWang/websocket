3b1790af23538d2cfc76fb75d1d540039817f778
fantast <1061505447@qq.com>
2020-12-24 17:22:27 +0800

    socket群发消息，不针对特定会话对象

--- src/main/java/com/example/websocket/config/SessionPool.java
+++ src/main/java/com/example/websocket/config/SessionPool.java
@@ -4,10 +4,16 @@ import javax.websocket.Session;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * @Author wangchaojie
+ * @Description 会话池
+ * @Date 8:53 2020/12/24
+ **/
 public class SessionPool {
-
+    //用于存储会话
     public static Map<String, Session> sessionMap = new ConcurrentHashMap<>();
 
+    //用于释放会话池中的对象
     public static void close(String sessionId) {
         for (String userId : sessionMap.keySet()) {
             Session session = sessionMap.get(userId);
@@ -18,12 +24,14 @@ public class SessionPool {
         }
     }
 
+    //群发：没有发向对应的sessionId
     public static void sendMessage(String msg) {
         for (String userId : sessionMap.keySet()) {
             sessionMap.get(userId).getAsyncRemote().sendText(msg);
         }
     }
 
+    //群发：没有发向对应的sessionId
     public static void sendMessage(String msg, String sessionId) {
         sessionMap.get(sessionId).getAsyncRemote().sendText(msg);
     }
